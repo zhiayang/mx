087ca5bcbe0c92c813f1ef8db98f7b5dafd0054a
Zhia Yang <zhiayang@gmail.com>
2014-09-04 01:41:53 +0800

    add pthread_join.cpp (which is useless at the mo)

---
+++ Libraries/libc/source/pthread/pthread_join.cpp
@@ -0,0 +1,16 @@
+// pthread_join.cpp
+// Copyright (c) 2014 - The Foreseeable Future, zhiayang@gmail.com
+// Licensed under the Apache License Version 2.0.
+
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <string.h>
+
+extern "C" int pthread_join(pthread_t thread, void** retval)
+{
+	void* ret = Library::SystemCall::GetRetVal(thread);
+	if(retval)
+		*retval = ret;
+
+	return 0;
+}
