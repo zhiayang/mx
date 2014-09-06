45c011221f7ef3da12ca021f1a9ea02abf17f03f
Zhia Yang <zhiayang@gmail.com>
2014-09-06 15:20:35 +0800

    another fix

--- tools/mountdisk.sh
+++ tools/mountdisk.sh
@@ -3,6 +3,6 @@
 if [ `uname` = "Darwin" ]; then
 	test -d /Volumes/mx || hdiutil attach -quiet build/disk.img
 elif [ `uname` = "Linux" ]; then
-	mkdir ../build/mnt/mx
-	mount ../build/disk.img ../build/mnt/mx
+	mkdir build/mnt/mx
+	mount build/disk.img build/mnt/mx
 fi
